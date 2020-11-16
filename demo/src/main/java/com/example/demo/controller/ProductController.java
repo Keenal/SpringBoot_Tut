ce519329f9cddee53cc8752b7dd6fa9c165053a2
Keenal <keenal.shah@ufl.edu>
2020-11-15 22:19:43 -0500

    adding to the database is working

--- demo/src/main/java/com/example/demo/controller/ProductController.java
+++ demo/src/main/java/com/example/demo/controller/ProductController.java
@@ -1,5 +1,10 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.demo.entities.Product;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +19,25 @@ public class ProductController {
     // Jackson converts POJO's to JSON
     @GetMapping("/product")
     public Product getProduct() {
-        
+        Product myProduct = new Product(1, "A", 10);
+        return myProduct;
     }
+
+    @GetMapping("/products")
+    public List<Product> getProducts() {
+        List<Product> myProducts = new ArrayList<>();
+        Product myProduct1 = new Product(1, "A", 10);
+        Product myProduct2 = new Product(1, "B", 30);
+        Product myProduct3 = new Product(1, "C", 30);
+
+        myProducts.add(myProduct1);
+        myProducts.add(myProduct2);
+        myProducts.add(myProduct3);
+        
+        return myProducts;
+    }
+
+    
     
     
 }
---
+++ demo/src/main/java/com/example/demo/entities/Product.java
@@ -0,0 +1,64 @@
+package com.example.demo.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String name;
+    private int price;
+
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", name='" + getName() + "'" +
+            ", price='" + getPrice() + "'" +
+            "}";
+    }
+
+
+    public Product(Integer id, String name, int price) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    
+
+
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+
+
+    
+}
